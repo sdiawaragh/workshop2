0c56f19bf59efd3b11c0ce6fdeb9b229c2e7321c
sdiawaragh <134128814+sdiawaragh@users.noreply.github.com>
2023-06-26 11:46:13 -0400

    first commit

--- app/app/nc_tutorials/settings.py
+++ app/app/nc_tutorials/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,9 +27,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'ec2-3-144-21-227.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,11 +124,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-else:
-    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
 
